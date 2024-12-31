fc36397badd4d2be6e48a5980248ceca86b65de2
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2024-12-31 16:48:58 +0100

    feat: Add event listener on buttons
    to handle button presses and run playRound.
    Next playRound needs to be adapted to handle buttons as new input.

--- js/javascript.js
+++ js/javascript.js
@@ -26,41 +26,11 @@ function getHumanChoice() {
     `);
 }
 function playGame(rounds) {
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(computerChoice, humanChoice) {
-        const humanChoiceLower = (humanChoice ?? "invalid input").toLowerCase();
-        const showChoices = `
-        You chose:      ${humanChoiceLower}
-        Computer chose: ${computerChoice}
-        
-        `;
-        if (humanChoiceLower === computerChoice) {
-            console.log(`${showChoices}Stalemate!`);
-        } else if (
-            (humanChoiceLower === "rock" && computerChoice === "scissors") ||
-            (humanChoiceLower === "paper" && computerChoice === "rock") ||
-            (humanChoiceLower === "scissors" && computerChoice === "paper")
-        ) {
-            console.log(`${showChoices}You win!`);
-            humanScore++;
-        } else if (
-            (humanChoiceLower === "rock" && computerChoice === "paper") ||
-            (humanChoiceLower === "paper" && computerChoice === "scissorsk") ||
-            (humanChoiceLower === "scissors" && computerChoice === "rock")
-        ) {
-            console.log(`${showChoices}You lose...`);
-            computerScore++;
-        } else {
-            console.log("Please enter valid input.");
-            return 1;
-        }
-    }
     console.log(`
         Welcome to Rock, Paper, Scissors!
         Win a duel against the computer!!!
-    `)
-/*     for (let i=1; i <= rounds; i++) {
+    `);
+    /*     for (let i=1; i <= rounds; i++) {
         let invalidInputCheck = playRound(getComputerChoice(), getHumanChoice());
         // if (invalidInputCheck === 1) {i--}
         console.log(`
@@ -79,16 +49,45 @@ function playGame(rounds) {
     } else {
         console.log("The game ends in a tie.");
     } */
-} 
+}
+function playRound(computerChoice, humanChoice) {
+    const humanChoiceLower = (humanChoice ?? "invalid input").toLowerCase();
+    const showChoices = `
+    You chose:      ${humanChoiceLower}
+    Computer chose: ${computerChoice}
+    
+    `;
+    if (humanChoiceLower === computerChoice) {
+        console.log(`${showChoices}Stalemate!`);
+    } else if (
+        (humanChoiceLower === "rock" && computerChoice === "scissors") ||
+        (humanChoiceLower === "paper" && computerChoice === "rock") ||
+        (humanChoiceLower === "scissors" && computerChoice === "paper")
+    ) {
+        console.log(`${showChoices}You win!`);
+        humanScore++;
+    } else if (
+        (humanChoiceLower === "rock" && computerChoice === "paper") ||
+        (humanChoiceLower === "paper" && computerChoice === "scissorsk") ||
+        (humanChoiceLower === "scissors" && computerChoice === "rock")
+    ) {
+        console.log(`${showChoices}You lose...`);
+        computerScore++;
+    } else {
+        console.log("Please enter valid input.");
+        return 1;
+    }
+}
 
-// EVLIST (button is clicked) -> 
-
-    // run playRound with "button corresponding" playerSelection
+let humanScore = 0;
+let computerScore = 0;
+let selectionButtons = Array.from(document.querySelectorAll(".selection-button"));
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener("click", playRound)
+});
     // RETURN results
 // ELEM create div
 // change clg's to something that shows as DOM text on the div
 // SHOW results of EVLIST on div. =>
     // SHOW running score
     // SHOW Winner (the one who wins 5 rounds / gets 5 points)
-
-playGame(2)
