7a305c46381dba080614be7d37b847fa9e377aea
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-01-01 15:45:09 +0100

    feat: Add 5 rounds playable

--- js/javascript.js
+++ js/javascript.js
@@ -10,32 +10,23 @@ function getHumanChoice() {
         (Because they do nothing...)
     `);
 } // WHEN output works on div, change this to show on div
-function playGame(rounds) {
-    console.log(`
+function playGame(humanChoiceButton) {
+    outputField.textContent = `
         Welcome to Rock, Paper, Scissors!
         Win a duel against the computer!!!
-    `);
-    /*     for (let i=1; i <= rounds; i++) {
-        let invalidInputCheck = playRound(getComputerChoice(), getHumanChoice());
-        // if (invalidInputCheck === 1) {i--}
-        console.log(`
-            current state:
-            
-            You:        ${humanScore}
-            Computer:   ${computerScore}
-
-            ${5-i} rounds left
-            `);
-    } 
-    if (humanScore > computerScore) {
-        console.log("You win the game!!!");
-    } else if (humanScore < computerScore) {
-        console.log("You lose the game...");
-    } else {
-        console.log("The game ends in a tie.");
-    } */
-} // WHEN div exists, show output from here on div
-// Up to this point, unused for now, because of refactoring
+    `;
+    playRound(humanChoiceButton);
+    if (humanScore >= 5) {
+        outputField.textContent = "You win the game!!!";
+        humanScore = 0;
+        computerScore = 0;
+    }
+    if (computerScore >= 5) {
+        outputField.textContent = "You lose the game...";
+        humanScore = 0;
+        computerScore = 0;
+    }
+}
 
 function getComputerChoice() {
     let computerChoiceInt = Math.ceil(Math.random() * 3);
@@ -53,8 +44,8 @@ function getComputerChoice() {
     }
 }
 function playRound(humanChoiceButton) {
-    const computerChoice = getComputerChoice()
-    const humanChoice = humanChoiceButton.target.textContent
+    const computerChoice = getComputerChoice();
+    const humanChoice = humanChoiceButton.target.textContent;
     const humanChoiceLower = (humanChoice ?? "invalid input").toLowerCase();
     const showChoices = `
     You chose:      ${humanChoiceLower}
@@ -62,39 +53,39 @@ function playRound(humanChoiceButton) {
     
     `;
     if (humanChoiceLower === computerChoice) {
-        outputField.textContent=(`${showChoices}Stalemate!`);
-        outputField.textContent += `human Score: ${humanScore}, computer Score: ${computerScore}`
+        outputField.textContent = `${showChoices}Stalemate!`;
+        outputField.textContent += `human Score: ${humanScore}, computer Score: ${computerScore}`;
     } else if (
         (humanChoiceLower === "rock" && computerChoice === "scissors") ||
         (humanChoiceLower === "paper" && computerChoice === "rock") ||
         (humanChoiceLower === "scissors" && computerChoice === "paper")
     ) {
-        outputField.textContent=(`${showChoices}You win!`);
+        outputField.textContent = `${showChoices}You win!`;
         humanScore++;
-        outputField.textContent += `human Score: ${humanScore}, computer Score: ${computerScore}`
+        outputField.textContent += `human Score: ${humanScore}, computer Score: ${computerScore}`;
     } else if (
         (humanChoiceLower === "rock" && computerChoice === "paper") ||
         (humanChoiceLower === "paper" && computerChoice === "scissors") ||
         (humanChoiceLower === "scissors" && computerChoice === "rock")
     ) {
-        outputField.textContent=(`${showChoices}You lose...`);
+        outputField.textContent = `${showChoices}You lose...`;
         computerScore++;
-        outputField.textContent += `human Score: ${humanScore}, computer Score: ${computerScore}`
+        outputField.textContent += `human Score: ${humanScore}, computer Score: ${computerScore}`;
     } else {
-        outputField.textContent=("Please enter valid input.");
+        outputField.textContent = "Please enter valid input.";
         return 1;
     }
 }
 
 let humanScore = 0;
 let computerScore = 0;
-let humanChoiceButtons = Array.from(document.querySelectorAll(".selection-button"));
+let humanChoiceButtons = Array.from(
+    document.querySelectorAll(".selection-button")
+);
 let outputField = document.querySelector(".output");
 
-humanChoiceButtons.forEach(humanChoiceButton => {
-    humanChoiceButton.addEventListener("click", playRound)
+humanChoiceButtons.forEach((humanChoiceButton) => {
+    humanChoiceButton.addEventListener("click", playGame);
 });
-if (humanScore >= 5 || computerScore >= 5) outputField.textContent = "Game is over!" 
-// SHOW results of EVLIST on div. =>
-    // SHOW running score
-    // SHOW Winner (the one who wins 5 rounds / gets 5 points)
+
+// SHOW Winner (the one who wins 5 rounds / gets 5 points)
